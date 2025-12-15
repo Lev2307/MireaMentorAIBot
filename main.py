7606736da2b17073b83a163a4d0625f272fd725d
Lev2307 <Unfortunately1366@gmail.com>
2025-12-15 22:28:42 +0300

    добавил прокси, теперь бот запускается

--- main.py
+++ main.py
@@ -3,21 +3,27 @@ import logging
 import sys
 import asyncio
 from aiogram import Bot, Dispatcher
-
-from dotenv import load_dotenv
+from aiogram.client.session.aiohttp import AiohttpSession
 
 from handlers.start import start_router
 from handlers.navigation import nav_router
 from handlers.schedule import schedule_router
 from handlers.question import question_router
 
+from dotenv import load_dotenv
 load_dotenv()
 
-dp = Dispatcher()
-
 async def main():
-    bot = Bot(token=os.environ.get("BOT_TOKEN"))
-    dp.include_routers(start_router, nav_router, schedule_router, question_router)
+    session = AiohttpSession(proxy=os.environ.get("PROXY_URL"))
+    bot = Bot(token=os.environ.get("BOT_TOKEN"), session=session)
+    dp = Dispatcher()
+
+    dp.include_routers(
+        start_router,
+        nav_router,
+        schedule_router,
+        question_router,
+    )
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
